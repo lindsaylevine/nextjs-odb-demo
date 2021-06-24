c8aa9499ea524957e344f784effdee289415f051
kenny-io <ekeneeze3@gmail.com>
2021-06-24 09:27:24 +0400

    updated url config

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-odb-demo.netlify.app/";
+  : "https://next-odb-demo.netlify.app";
